613c73448aec6c26871bd89534c6c99efe56b447
Daniel Fraga <dan.fraga33@gmail.com>
2025-08-03 10:15:52 +1000

    fix: make responsive to mobile

--- frontend/app/components/strategy.tsx
+++ frontend/app/components/strategy.tsx
@@ -3,9 +3,16 @@ import Big_TA from "./big_ta";
 
 const Strategy = () => {
   return (
-    <div className="flex items-center justify-around">
-      <Rules />
-      <Big_TA />
+    <div className="mx-auto flex w-full max-w-7xl flex-col justify-evenly gap-6 p-4 lg:flex-row">
+      {/* Rules Component */}
+      <div className="w-full flex-1 lg:w-1/2">
+        <Rules />
+      </div>
+
+      {/* Big_TA Component */}
+      <div className="w-full flex-1 lg:w-1/2">
+        <Big_TA />
+      </div>
     </div>
   );
 };
